960d12250ffd78f45092f5849e9fec5c5491adce
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-07 21:37:04 -0500

    Crear verificación de parametros y no solicitado

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
-let nombres = {
-    Nombre1: "Diana",
-    Nombre2: "Marcela",
-    Nombre3: "Nestor", 
-    Nombre4: "Maria",
-};
-let nombres2 = ["Diana","Marcela","Nestor", "Maria"];
+function Saludar(lista) {
+    let {nombre, direccion, edad, ...DatosSobrantes} = lista;
+    lista = undefined;
+}
 
-let fnA = (Alumnos) => {
-    Object.assign(this, Alumnos);
-    console.log(this.Nombre1);
-    console.log(this.Nombre2);
-    console.log(this.Nombre3);
-    console.log(this.Nombre4);
-};
-let fnB = (Nombre1, Nombre2, Nombre3, Nombre4) => {
-    console.log(Nombre1);
-    console.log(Nombre2);
-    console.log(Nombre3);
-    console.log(Nombre4);
-};
-fnA(nombres);
-console.log("-------------------------------------");
-fnB(...nombres2);
+Saludar({nombre:'Diana', direccion:"Calle 55", altura: 1.70});
+
+
+
+// let nombres = {
+//     Nombre1: "Diana",
+//     Nombre2: "Marcela",
+//     Nombre3: "Nestor", 
+//     Nombre4: "Maria",
+// };
+// let nombres2 = ["Diana","Marcela","Nestor", "Maria"];
+
+// let fnA = (Alumnos) => {
+//     Object.assign(this, Alumnos);
+//     console.log(this.Nombre1);
+//     console.log(this.Nombre2);
+//     console.log(this.Nombre3);
+//     console.log(this.Nombre4);
+// };
+// let fnB = (Nombre1, Nombre2, Nombre3, Nombre4) => {
+//     console.log(Nombre1);
+//     console.log(Nombre2);
+//     console.log(Nombre3);
+//     console.log(Nombre4);
+// };
+// fnA(nombres);
+// console.log("-------------------------------------");
+// fnB(...nombres2);
 
 // http://www.etnassoft.com/2016/06/22/las-funciones-flecha-en-javascript-parte-1/
 // Funciones Flecha
